b2cb813f3f38a63fdb66582c44f99a904c93762c
linam2021 <lina.m.2021.m@gmail.com>
2021-04-19 02:52:34 +0300

    change more

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 	<!-- General Head Content -->
 	<!-- meta tags -->
 	<meta charset="utf-8" />
-	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' /><![endif]-->
+	<meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' />
 
 	<meta name="description" content="A beautiful bootstrap template for your small business, company, agency or corporate website" />
 
