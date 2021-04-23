75753593d355b33ea5ed86d6420e8ee7a23e4300
linam2021 <lina.m.2021.m@gmail.com>
2021-04-23 17:36:26 +0300

    new changes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<!-- General Head Content -->
 	<!-- meta tags -->
-	<meta charset="utf-8" />
+	<meta charset="utf-8">
 	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1' /><![endif]-->
 
 	<meta name="description" content="The Game Academy Website" />
